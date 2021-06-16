ee5858942aa8adef2981ebdca93b8e12200aaceb
leonardo jaques <jaques.projetos@outlook.com>
2021-06-16 14:26:11 -0300

    Iserted footer in the page

--- src/app/components/Logo/index.tsx
+++ src/app/components/Logo/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import CarLogoImage from '../../../assets/images/car-logo.png';
+import CarLogoDarkImg from '../../../assets/images/car-logo-dark.png';
+import CarLogoImg from '../../../assets/images/car-logo.png';
+
+interface ILogoProps {
+  color?: 'white' | 'dark';
+  bgColor?: 'white' | 'dark';
+}
 
 const LogoContainer = styled.div`
   ${tw`
@@ -17,9 +23,9 @@ const LogoText = styled.div`
     font-bold
     text-black
     m-1
-
-`}
-`;
+    `}
+  ${({ color }: any) => (color === 'white' ? tw`text-white` : tw`text-black`)}
+` as any;
 
 const Image = styled.div`
   width: auto;
@@ -31,13 +37,14 @@ const Image = styled.div`
   }
 `;
 
-export function Logo() {
+export function Logo(props: ILogoProps) {
+  const { color, bgColor } = props;
   return (
     <LogoContainer>
       <Image>
-        <img src={CarLogoImage} />
+        <img src={bgColor === 'dark' ? CarLogoDarkImg : CarLogoImg} />
       </Image>
-      <LogoText>Yourcar.</LogoText>
+      <LogoText color={color || 'dark'}>Yourcar.</LogoText>
     </LogoContainer>
   );
 }
