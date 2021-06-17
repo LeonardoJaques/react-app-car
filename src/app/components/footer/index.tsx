6ba419aeba18f86dc020150dca9ddeb1cf178006
leonardo jaques <jaques.projetos@outlook.com>
2021-06-17 09:46:39 -0300

    Finished Design of Footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,18 +1,23 @@
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import {
+  faEnvelopeSquare,
+  faPhoneAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import { Logo } from '../Logo';
 
 const FooterContainer = styled.div`
-  min-height: 30em;
+  /* min-height: 24em; */
   background-color: #1d2124;
   ${tw`
   flex
-
+  flex-col
   min-w-full
-  pt-4
-  md:pt-12
-  pb-1
+  pt-10
+  md:pt-16
   items-center
   justify-center
 `}
@@ -21,10 +26,11 @@ const FooterContainer = styled.div`
 const InnerContainer = styled.div`
   ${tw`
   flex
-  flex-col
+  flex-wrap
   w-full
   h-full
   max-w-screen-2xl
+
 `}
 `;
 
@@ -32,7 +38,10 @@ const AboutContainer = styled.div`
   ${tw`
   flex
   flex-col
-`}
+  mr-2
+  md:mr-16
+  pl-10
+  `}
 `;
 
 const AboutText = styled.p`
@@ -45,6 +54,105 @@ const AboutText = styled.p`
     mt-2
   `}
 `;
+
+const SectionContainer = styled.div`
+  ${tw`
+  w-full
+  md:w-auto
+  flex
+  flex-col
+  mr-2
+  md:mr-16
+  pl-10
+  pr-10
+  md:pl-3
+  md:pr-3
+  mt-7
+  md:mt-0
+  `}
+`;
+
+const ListItem = styled.li`
+  ${tw`
+    mb-3
+  `}
+  & > a {
+    ${tw`
+      text-sm
+      text-white
+      transition-all
+      hover:text-gray-300
+    `}
+  }
+`;
+
+const HeaderTitle = styled.h3`
+  ${tw`
+    text-2xl
+    font-bold
+    text-white
+    mb-3
+`}
+`;
+
+const LinksList = styled.ul`
+  ${tw`
+    outline-none
+    list-none
+    flex
+    flex-col
+  `}
+`;
+
+const HorizontalContainer = styled.div`
+  ${tw`
+    flex
+    items-center
+  `}
+`;
+
+const RedIcon = styled.span`
+  ${tw`
+    w-9
+    h-9
+    rounded-full
+    bg-red-500
+    flex
+    items-center
+    justify-center
+    text-white
+    text-base
+    mr-2
+  `}
+`;
+
+const SmallText = styled.h6`
+  ${tw`
+    inline-flex
+    text-sm
+    text-white
+  `}
+`;
+
+const BottomContainer = styled.div`
+  ${tw`
+    w-full
+    flex
+    max-w-screen-2xl
+    justify-center
+    md:justify-start
+    mt-7
+    md:mt-1
+  `}
+`;
+
+const CopyrightText = styled.small`
+  font-size: 12px;
+  ${tw`
+    text-gray-300
+  `}
+`;
+
 function Footer() {
   return (
     <FooterContainer>
@@ -56,7 +164,71 @@ function Footer() {
             country across the word which has cars and top rated service.
           </AboutText>
         </AboutContainer>
+        <SectionContainer>
+          <HeaderTitle>Our Links</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="#">Home</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">About Us</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Services</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Models</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Blog</a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Other Links</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="#">FAQ</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Contact Us</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Support</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Privacy Policy</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Terms &amp; Conditions</a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Call Now</HeaderTitle>
+          <HorizontalContainer>
+            <RedIcon>
+              <FontAwesomeIcon icon={faPhoneAlt} />
+            </RedIcon>
+            <SmallText>+55 555-234-8468</SmallText>
+          </HorizontalContainer>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Mail</HeaderTitle>
+          <HorizontalContainer>
+            <RedIcon>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </RedIcon>
+            <SmallText>info@yourcar.com</SmallText>
+          </HorizontalContainer>
+        </SectionContainer>
       </InnerContainer>
+      <BottomContainer>
+        <CopyrightText>
+          Copyright &copy; {new Date().getFullYear()} Yourcar. All right
+          reserved.
+        </CopyrightText>
+      </BottomContainer>
     </FooterContainer>
   );
 }
